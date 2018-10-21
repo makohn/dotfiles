af687958a1193345156fe8b279ba49ebcf4aa5c9
Marek Kohn <me@makohn.de>
2018-10-21 18:51:04 +0200

    Add init script

---
+++ init.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Ask for the admin password upfront
+sudo -v
+
+# Symlink files
+ln -s /Users/mkohn/dotfiles/.bash_profile /Users/mkohn/.bash_profile
+ln -s /Users/mkohn/dotfiles/.gitconfig /Users/mkohn/.gitconfig
+ln -s /Users/mkohn/dotfiles/.vimrc /Users/mkohn/.vimrc
+ln -s /Users/mkohn/dotfiles/.inputrc /Users/mkohn/.inputrc
