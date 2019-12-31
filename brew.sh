cb883f08cc7aacfc4542f9b747a078197b9e0137
Marek Kohn <me@makohn.de>
2019-12-31 21:40:50 +0100

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -52,3 +52,6 @@ ln -s "/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code" /
 
 # subl -> Sublime Text
 ln -s "/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
+
+# smerge -> Sublime Merge
+ln -s "/Applications/Sublime\ Merge.app/Contents/SharedSupport/bin/smerge" /usr/local/bin/smerge
